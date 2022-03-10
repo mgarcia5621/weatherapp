23aa64d21cc82b8e3dd58cec94f13af618f21cfa
Mateo <matthew27garcia@gmail.com>
2022-03-10 12:29:44 -0600

    created the fetch response when logged

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 let weather = {
     "apiKey": "fc711781e117e5777f4dba612ee8d94d",
     fetchWeather: function() {
-        fetch("")
+        fetch("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=fc711781e117e5777f4dba612ee8d94d"
+        ).then((repsonse) => repsonse.json())
+        .then((data) => console.log(data));
     }
 };
 
