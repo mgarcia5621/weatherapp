0106327a4ad59099fb541ce8dc7b9f63e377455d
Mateo <matthew27garcia@gmail.com>
2022-03-10 13:14:00 -0600

    added the javascript that inputs in the innerhtml to display weather

--- scripts.js
+++ scripts.js
@@ -1,9 +1,20 @@
 let weather = {
     "apiKey": "fc711781e117e5777f4dba612ee8d94d",
     fetchWeather: function() {
-        fetch("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=fc711781e117e5777f4dba612ee8d94d"
+        fetch("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid="
+        +city
+        +this.apiKey
         ).then((repsonse) => repsonse.json())
-        .then((data) => console.log(data));
+        .then((data) => this.displayWeather(data));
+
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector("city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/01n@2x.png"
     }
 };
 
