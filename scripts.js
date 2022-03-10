aabe482310b685b441605d70ffc65704d5f67433
Mateo <matthew27garcia@gmail.com>
2022-03-10 15:58:13 -0600

    fixed and aligned the items of the weather in the correct placement

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
 let weather = {
     "apiKey": "fc711781e117e5777f4dba612ee8d94d",
-    fetchWeather: function() {
-        fetch("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid="
-        +city
-        +this.apiKey
+    fetchWeather: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
         ).then((repsonse) => repsonse.json())
-        .then((data) => this.displayWeather(data));
+        .then((data) => {
+            this.displayWeather(data);
+        });
 
     },
     displayWeather: function(data) {
@@ -13,8 +16,14 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        document.querySelector("city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/01n@2x.png"
+        console.log(name, icon, description, temp, humidity);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + "@2x.png";
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = humidity + "%";
+        document.querySelector(".wind").innerText = speed + " km/h";
     }
 };
+
+weather.fetchWeather("Cibolo");
 
