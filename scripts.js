5a06d6a33079b7c46a073ae21d31dade31254805
Mateo <matthew27garcia@gmail.com>
2022-03-11 16:22:21 -0600

    fixed the enter button to tab

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 let weather = {
     "apiKey": "fc711781e117e5777f4dba612ee8d94d",
-    fetchWeather: function(city) {
+    fetchWeather: function(city, lon, lat) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
         "&units=metric&appid=" +
@@ -9,17 +9,26 @@ let weather = {
         .then((data) => {
             this.displayWeather(data);
         });
-
+    },    
+    getGeoPosition: function() {
+        navigator.geolocation.getCurrentPosition(function (position) {
+        console.log(position.coords.latitude)
+        console.log(position.coords.longitude)
+        },
+        function (error) {
+            console.log("The Locator was denied. :(")
+        })
     },
     displayWeather: function(data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
+        let fahrenheit = Math.round((temp * 9/5) + 32);
         console.log(name, icon, description, temp, humidity);
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + "@2x.png";
-        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".temp").innerText = fahrenheit + "°F";
         document.querySelector(".humidity").innerText = humidity + "%";
         document.querySelector(".wind").innerText = speed + " km/h";
         document.querySelector(".description").innerText = description + " km/h";
@@ -34,9 +43,10 @@ document.querySelector(".search button")
     weather.search();
 })
 
-document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+document.querySelector(".search-bar").addEventListener("keydown", function (event) {
     if (event == "Enter") {
         weather.search();
     }
 });
 
+weather.getGeoPosition();
